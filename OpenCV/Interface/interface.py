e68f87526c4988e4140691b59e96d470049ed57d
Eric Kang <97197222+erickdhdock@users.noreply.github.com>
2023-04-12 09:29:23 -0700

    update

--- OpenCV/Interface/interface.py
+++ OpenCV/Interface/interface.py
@@ -16,7 +16,7 @@ def searchPortCam():
         if not cap.read()[0]:
             break
         else:
-            arr.append(index)
+            arr.append(str(f'{index}'))
         cap.release()
         index += 1
     return arr
@@ -38,24 +38,22 @@ def searchSerialPort():
     print(HWID[0])
     return HWID
 
-camport = None
-comport = None
 
-def saver(var, var1):
-    global camport, comport
-    comport = var
-    camport = var1
-    print(f'selected camport is {camport}')
-    camera_Setting(camPort= camport)
+def saver(cam, com, len):
+    i = 0
+    if len > 1:
+        while i < len:
+            camera_Setting(camPort= cam[i])
+            i+=1
+    else:
+        camera_Setting(camPort= cam)
+    
     
   
-
-def initSave(camPort, comPort):
-    camera = int(camPort)
-    comunication = comPort
     
 
 def __initiate__():
+    delta = 0
     camPorts = searchPortCam()
     comPorts = searchSerialPort()
     ay =  100
@@ -67,41 +65,44 @@ def __initiate__():
     window.geometry("600x240")
     window.resizable(True,True)
     
-    
-    
-    camList_val = tk.StringVar(window, "Cam Port")
-    camList_val.set("Cam Port")
     comList_val = tk.StringVar(window, "Com Port")
     comList_val.set("Com Port")
-    
-    if len(camPorts)>1:
-        delta = 0
-        variable = {}
-        variableText = {}
-        print(f'cam port number greater than 0 // {len(camPorts)}')
-        while len(camPorts)>= delta:
-            name = f"camLable{delta}"
-            variableText[name] = tk.Label(window,text=f'Select Camera {delta} Port', font=('Arial',15))
-            variableText[name].place(x=30, y = ayl +(45*delta))
-            variable[name] = tk.OptionMenu(window, camList_val, *camPorts)
-            variable[name].place(x=30, y= ay+(45*delta))
-            delta+=1
-        btn = tk.Button(window, text='Next', command=lambda: saver(comList_val, camList_val))
-        btn.place( x= 40, y= (ay+ayl+45) )
-    else:
-        CamPortLabel = tk.Label(window, text='Select Camera 1 Port', font=('Arial',15))
-        CamPortLabel.place(x=30, y=ayl)
-        btn = tk.Button(window, text='Next', command=lambda: saver(comList_val.get(), camList_val.get()))
-        btn.place(x= 40, y= 130)
-    
-    
-    camOption = tk.OptionMenu(window, camList_val, *camPorts)
-    camOption.place(x=30, y=ay)
-    
     ComLable = tk.Label(window, text='Select Communication Port', font=('Arial', 15))
     ComLable.place(x=30,y=20)
     ComOption = tk.OptionMenu(window, comList_val, *comPorts)
     ComOption.place(x= 30, y= 45)
+        
+    
+    if len(camPorts) > 1:
+        camList_val = {}
+        variableText = {}
+        variable = {}
+        holder = {}
+        print(f'cam port number greater than 0 // {len(camPorts)}')
+        while len(camPorts) > delta:
+            name = f"{delta}"
+            camList_val[name] = tk.StringVar(window, "Cam Port")
+            camList_val[name].set("Cam Port")
+            variableText[name] = tk.Label(window,text=f'Select Camera {delta+1} Port', font=('Arial',15))
+            variableText[name].place(x=30, y= ayl + (45*delta))
+            variable[name] = tk.OptionMenu(window, camList_val[name], *camPorts)
+            variable[name].place(x=30, y= ay + (45*delta))
+            holder[delta] = camList_val[name].get()
+            delta += 1
+
+        btn = tk.Button(window, text='Next', command=lambda: saver(holder, comList_val.get(), len(camPorts)))
+        btn.place(x= 40, y= (ay+ayl+45))
+    else:
+        CamPortLabel = tk.Label(window, text='Select Camera 1 Port', font=('Arial',15))
+        CamPortLabel.place(x=30, y=ayl)
+        camList_val = tk.StringVar(window, "Cam Port")
+        camList_val.set("Cam Port")
+        variable = tk.OptionMenu(window, camList_val, *camPorts)
+        variable.place(x=30, y= ay)
+        btn = tk.Button(window, text='Next', command=lambda: saver(camList_val.get(), comList_val.get(), len(camPorts)))
+        btn.place(x= 40, y= 130)
+    
+    
     
     
 
@@ -122,33 +123,33 @@ def colourInterface():
 
 def camVid(frontVid):
     read, standard = frontVid.read()
-    standard1 = cv2.cvtColor(standard, cv2.COLOR_BGR2RGB)
-        # Convert the image to PIL format
-    pil_img = Image.fromarray(standard1)
+    # standard1 = cv2.cvtColor(standard, cv2.COLOR_BGR2RGB)
+    #     # Convert the image to PIL format
+    # pil_img = Image.fromarray(standard1)
 
-    # Convert the PIL image to a PIL.ImageTk.PhotoImage object
-    photo_img = ImageTk.PhotoImage(pil_img)
+    # # Convert the PIL image to a PIL.ImageTk.PhotoImage object
+    # photo_img = ImageTk.PhotoImage(pil_img)
 
-    return photo_img
-    
+    # return photo_img
+    return standard
 
 def __camInit__(selectedport, len, resolutionX, resolutionY):
+    passed = False
+    
     if len == 1:
         frontVid = cv2.VideoCapture(selectedport)
+        resolutionX = float(resolutionX)
+        resolutionY = float(resolutionY)
         
         if frontVid.isOpened():
             print('[LOG]: frontvid is open')
             frontVid.set(cv2.CAP_PROP_FRAME_WIDTH, resolutionX)
             frontVid.set(cv2.CAP_PROP_FRAME_HEIGHT, resolutionY)
+            passed = True
         else:
             tk.messagebox.showinfo(title='Error', message='Front Camera encountered problem')
             exit()
             
-            
-        while True:
-            camVid(frontVid)
-        
-
 
     else:
         i=0
@@ -159,15 +160,19 @@ def __camInit__(selectedport, len, resolutionX, resolutionY):
             if not svr[name].isOpened():
                 tk.messagebox.showinfo(title='Error', message=f'Camera {name} encountered problem')
             else:
-                svr[name].set(cv2.CAP_PROP_FRAME_WIDTH, resolutionX)
-                svr[name].set(cv2.CAP_PROP_FRAME_HEIGHT, resolutionY)
+                svr[name].set(cv2.CAP_PROP_FRAME_WIDTH, float(resolutionX))
+                svr[name].set(cv2.CAP_PROP_FRAME_HEIGHT, float(resolutionY))
                 i+=1
+    if passed == True:
+        alpha = camVid(frontVid)
+        return alpha
     
 def camDisplayer (selectedPort, len, resolutionX, resolutionY):
     selectedPort = int(selectedPort)
     while True:
         cv2.imshow('display',__camInit__(selectedPort, len, resolutionX, resolutionY))
-    
+        if cv2.waitKey(1) == ord('q'):
+            break
         
 def camera_Setting(camPort):
     global window
@@ -184,17 +189,18 @@ def camera_Setting(camPort):
     infoRes = tk.Label(camWindow, text='Width x Height for camera')
     infoRes.place(x=30, y=5)
     leftX = tk.Entry(camWindow, width=4)
+    leftX.setvar(1000)
     leftX.place(x=30, y=30)
     rightX = tk.Entry(camWindow, width=4)
+    rightX.setvar(1000)
     rightX.place(x=90,y=30)
     x = tk.Label(camWindow, text='X')
     x.place(x=79, y=33)
     
     
     
-    btn = tk.Button(camWindow, text='search')
+    btn = tk.Button(camWindow, text='search', command=camDisplayer(camPort, len(arr), leftX.get(), rightX.get()))
     btn.place(x= 30, y= 67)
-    btn.config(command=camDisplayer(camPort, len(arr), leftX, rightX))
     
     
     camWindow.mainloop()
