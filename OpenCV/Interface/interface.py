2223e3d9b16a68c42caff63069542392902cc5dc
Eric Kang <97197222+erickdhdock@users.noreply.github.com>
2023-04-15 17:54:39 -0700

    interfacy py works

--- OpenCV/Interface/interface.py
+++ OpenCV/Interface/interface.py
@@ -4,11 +4,94 @@ import cv2
 import serial.tools.list_ports
 import serial
 from PIL import Image, ImageTk
-import time
+import subprocess
 
 
+# Create global variables
 debugVar = 0
+passed = False
+inform = 0
+HWID_DEVICE = []
+HWID = []
 
+def dicCheck(dictionary, chkVal):
+    current = 0
+    while current < len(dictionary):
+        if not chkVal == dictionary[current]:
+            current += 1
+        else:
+            print(current)
+            break
+    return current 
+
+# Step 1: 1st screen, select camport and comport
+def __initiate__():
+    # create variables
+    global window
+    delta = 0 # count how many cameras there are
+    camPorts = searchPortCam() # search comports
+    comPorts = searchSerialPort() # search serial ports
+    baudRates = [300,1200,2400,9600,10417,19200,57600,115200]
+    ax = 30 # default x location for camera entry
+    ay =  150 # default location for camera entry
+    ayl = 130 # default location for camera entry explanation
+    
+    # create window using tkinter
+    window = tk.Tk()
+    window.title("Project Claw For Humanity Port Selector")
+    window.geometry("600x240")
+    window.resizable(True,True)
+    
+    # Select comport and camport
+    comList_val = tk.StringVar(window, "Com Port")
+    comList_val.set("Com Port")
+    ComLable = tk.Label(window, text='Select Communication Port', font=('Arial', 15))
+    ComLable.place(x=ax,y=20)
+    ComOption = tk.OptionMenu(window, comList_val, *comPorts)
+    ComOption.place(x= ax, y= 45)
+    
+    baud_val = tk.StringVar(window,"Baud Rate")
+    baud_val.set("Baud Rate")
+    baudLable = tk.Label(window, text="Select Baud Rate", font=('Arial', 15))  
+    baudLable.place(x= ax, y= 80)    
+    baudOption = tk.OptionMenu(window, baud_val, *baudRates)
+    baudOption.place(x= ax, y= 100)
+    
+    # happens only if there are more than 1 camera 
+    if len(camPorts) > 1:
+        # create local variables
+        camList_val = {}
+        variableText = {}
+        variable = {}
+        holder = {}
+        
+        # happens only if there are more than 1 camera
+        while len(camPorts) > delta:
+            name = f"{delta}"
+            camList_val[name] = tk.StringVar(window, "Cam Port")
+            camList_val[name].set("Cam Port")
+            variableText[name] = tk.Label(window,text=f'Select Camera {delta+1} Port', font=('Arial',15))
+            variableText[name].place(x=ax, y= ayl + (45*delta))
+            variable[name] = tk.OptionMenu(window, camList_val[name], *camPorts)
+            variable[name].place(x=ax, y= ay + (45*delta))
+            holder[delta] = camList_val[name].get()
+            delta += 1
+        # button calls saver
+        btn = tk.Button(window, text='Next', command=lambda: camera_Setting(holder, comList_val.get(), baud_val.get()))
+        btn.place(x= ax+10, y= (ay+ayl+45))
+    else:
+        CamPortLabel = tk.Label(window, text='Select Camera 1 Port', font=('Arial',15))
+        CamPortLabel.place(x=ax, y=ayl)
+        camList_val = tk.StringVar(window, "Cam Port")
+        camList_val.set("Cam Port")
+        variable = tk.OptionMenu(window, camList_val, *camPorts)
+        variable.place(x=ax, y= ay)
+        btn = tk.Button(window, text='Next', command=lambda: camera_Setting(camList_val.get(), comList_val.get(),baud_val.get()))
+        btn.place(x= ax+10, y= 200)
+    
+    window.mainloop()
+
+# for __initiate__() // search comport and camport
 def searchPortCam():
     index = 0
     global arr
@@ -22,123 +105,105 @@ def searchPortCam():
         cap.release()
         index += 1
     return arr
-
 def searchSerialPort():
     #variable
-    HWID = []
+    global HWID_DEVICE, HWID
     i=0
     ports = list(serial.tools.list_ports.comports())
-    
+
     if len(ports) == 0:
         HWID.append("No Ports Available")
     else:
         while i<len(ports):
             port = ports[i]
+            HWID_DEVICE.append(port.device)
             HWID.append(f"Name: {port.device} || Description: {port.description}")
             i+=1
-    
-    print(HWID[0])
     return HWID
 
-def saver(cam, com, len):
-    serialCom = com
-    
-    i = 0
-    if len > 1:
-        while i < len:
-            camera_Setting(camPort= cam[i])
-            i+=1
+# setting for camera -> set resolution, camera preview
+def camera_Setting(camPort, comPort, bdrate):
+    # tempo work
+    tempo = cv2.VideoCapture(int(camPort))
+
+    if not tempo.isOpened():
+        tempo = cv2.VideoCapture(int(camPort))
+        camera_Setting(int(camPort), comPort, bdrate)
     else:
-        camera_Setting(camPort= cam)  
+        frame_width = int(tempo.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(tempo.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
-def __initiate__():
-    delta = 0
-    camPorts = searchPortCam()
-    comPorts = searchSerialPort()
-    ay =  100
-    ayl = 80
-    
+    # kill window
     global window
-    window = tk.Tk()
-    window.title("Project Claw For Humanity Port Selector")
-    window.geometry("600x240")
-    window.resizable(True,True)
+    window.destroy()
     
-    comList_val = tk.StringVar(window, "Com Port")
-    comList_val.set("Com Port")
-    ComLable = tk.Label(window, text='Select Communication Port', font=('Arial', 15))
-    ComLable.place(x=30,y=20)
-    ComOption = tk.OptionMenu(window, comList_val, *comPorts)
-    ComOption.place(x= 30, y= 45)
-        
+    # create window
+    global camWindow
+    camWindow = tk.Tk()
+    camWindow.title("Camera Setting")
+    camWindow.option_add("*Font","Ariel 15")
+    camWindow.geometry("1000x500")
+    camWindow.resizable(False, False)
     
-    if len(camPorts) > 1:
-        camList_val = {}
-        variableText = {}
-        variable = {}
-        holder = {}
-        print(f'cam port number greater than 0 // {len(camPorts)}')
-        while len(camPorts) > delta:
-            name = f"{delta}"
-            camList_val[name] = tk.StringVar(window, "Cam Port")
-            camList_val[name].set("Cam Port")
-            variableText[name] = tk.Label(window,text=f'Select Camera {delta+1} Port', font=('Arial',15))
-            variableText[name].place(x=30, y= ayl + (45*delta))
-            variable[name] = tk.OptionMenu(window, camList_val[name], *camPorts)
-            variable[name].place(x=30, y= ay + (45*delta))
-            holder[delta] = camList_val[name].get()
-            delta += 1
+    
+    # entry
+    infoRes = tk.Label(camWindow, text='Width x Height for camera')
+    infoRes.place(x=30, y=5)
+    
+    leftX = tk.Entry(camWindow, width=4)
+    leftX.place(x=30, y=30)
+    
+    rightX = tk.Entry(camWindow, width=4)
+    rightX.place(x=90,y=30)
+    
+    x = tk.Label(camWindow, text='X')
+    x.place(x=79, y=33)
+    
+    recRes = tk.Label(camWindow, text=f'recommended camera resolution is {frame_width}x{frame_height}')
+    recRes.place(x= 113, y= 69)
+    
+    btn = tk.Button(camWindow, text='search', command=lambda: camDisplayer(camPort, 1, leftX.get(), rightX.get(),camWindow, comPort, bdrate))
+    btn.place(x= 30, y= 67)
+    
+    
+    camWindow.mainloop()
 
-        btn = tk.Button(window, text='Next', command=lambda: saver(holder, comList_val.get(), len(camPorts)))
-        btn.place(x= 40, y= (ay+ayl+45))
+def camDisplayer(camPort, len, resolutionX, resolutionY, windo, comport, bdrate):
+    resolutionX = int(resolutionX)
+    resolutionY = int(resolutionY)
+    global inform
+    ret = __camInit__(int(camPort), len, resolutionX, resolutionY)
+    if inform == 0:
+        ret = __camInit__(int(camPort), len, resolutionX, resolutionY)
+        inform += 1
     else:
-        CamPortLabel = tk.Label(window, text='Select Camera 1 Port', font=('Arial',15))
-        CamPortLabel.place(x=30, y=ayl)
-        camList_val = tk.StringVar(window, "Cam Port")
-        camList_val.set("Cam Port")
-        variable = tk.OptionMenu(window, camList_val, *camPorts)
-        variable.place(x=30, y= ay)
-        btn = tk.Button(window, text='Next', command=lambda: saver(camList_val.get(), comList_val.get(), len(camPorts)))
-        btn.place(x= 40, y= 130)
+        pass
     
+    if resolutionX or resolutionY > 1000:
+        resolutionXT = int(resolutionX) / 2
+        resolutionYT = int(resolutionY) / 2
+    else: 
+        resolutionXT = resolutionX
+        resolutionYT = resolutionY
     
+    canvas = tk.Canvas(windo, width=resolutionXT, height=resolutionYT)
+    canvas.pack()
+    canvas_image = canvas.create_image(0, 50, anchor=tk.NW)
+
+    comCall = tk.Button(windo, text='next', command=lambda: __initCom__(comport, bdrate))
+    comCall.place(x= 400, y= 300)
+
+    # update the video in a loop
+    def update_loop():
+        ret = __camInit__(int(camPort), len, resolutionXT, resolutionYT)
+        update_video(ret, canvas, canvas_image, resolutionXT, resolutionYT)
+        # call this function again after 10ms
+        windo.after(10, update_loop)
+    update_loop()
     
-    
-
-    window.mainloop()
-        
-def colourInterface():
-    ColourWindow = tk.Tk()
-    ColourWindow.title("Project Claw For Humanity Main")
-    ColourWindow.geometry("1000x800")
-    ColourWindow.resizable(True,True)
-    # colours and buttons
-    redBtn = tk.Button(ColourWindow, text='red')
-    redBtn.place(x= 100, y= 100)
-    bluBtn = tk.Button(ColourWindow, text='blue')
-    bluBtn.place(x= 200, y= 100)
-    yelBtn = tk.Button(ColourWindow, text = 'green')
-    yelBtn.place()
-
-def camVid(camWindow, standard, resolX, resolY):
-    canvas = tk.Canvas(camWindow, width= resolX, height= resolY)
-    canvas.place(x= 300, y= 300)
-    while True:
-        standard1 = cv2.cvtColor(standard, cv2.COLOR_BGR2RGB)
-            # Convert the image to PIL format
-        pil_img = Image.fromarray(standard1)
-
-        # Convert the PIL image to a PIL.ImageTk.PhotoImage object
-        photo_img = ImageTk.PhotoImage(pil_img)
-
-        canvas.create_image(300,300, anchor ='nw', Image = photo_img)
-
-        # return photo_img
-
-passed = False
 def __camInit__(selectedport, len, resolutionX, resolutionY):
     global passed, frontVid
-    print(passed)
+    
     if passed == False:
         if len == 1:
             frontVid = cv2.VideoCapture(selectedport)
@@ -161,13 +226,10 @@ def __camInit__(selectedport, len, resolutionX, resolutionY):
                 return 'error'
                     
     elif passed == True:
-        print(f'entered elif statement passed state is {passed}\n')
-   
         read, standard = frontVid.read()
         return standard
-        
 
-def update_video(ret, window, canvas, canvas_image,resX,resY):
+def update_video(ret, canvas, canvas_image,resX,resY):
     # Convert the frame from BGR to RGB
     frame = cv2.cvtColor(ret, cv2.COLOR_BGR2RGB)
 
@@ -184,66 +246,123 @@ def update_video(ret, window, canvas, canvas_image,resX,resY):
     canvas.itemconfig(canvas_image, image=imgtk)
     canvas.image = imgtk
 
-inform = 0
-def camDisplayer(selectedPort, len, resolutionX, resolutionY, windo):
-    global inform
-    ret = __camInit__(int(selectedPort), len, resolutionX, resolutionY)
+state = None
 
-    if inform == 0:
-        ret = __camInit__(int(selectedPort), len, resolutionX, resolutionY)
-        inform += 1
+serialInst = None
 
+def __initCom__(comPort, baudrate):
+    global camWindow,state
+    state = 'checking connection'
+    camWindow.destroy()
+    
+    global comWindow
+    comWindow = tk.Tk()
+    comWindow.title("ComPort checker")
+    comWindow.option_add("*Font","Ariel 15")
+    comWindow.geometry("1000x500")
+    comWindow.resizable(False, False)
+    
+    where = dicCheck(HWID, comPort)
+    portInfo = tk.Label(comWindow, text=f'comport connection check // selected comport is {HWID_DEVICE[where]} // selected baud rate is {baudrate}')
+    portInfo.place(x=30, y=15)
+    
+    stateInfo = tk.Label(comWindow, text=f'current state is {state}')
+    stateInfo.place(x=30, y=50)
+    
+    cntBtn = tk.Button(comWindow, text= 'Connect', command=lambda: connect(HWID_DEVICE[where], baudrate))
+    
+    cntBtn.place(x= 30, y= 90)
+    
+    if state == 'connected':
+        print('connected')
+        nxtBtn = tk.Button(comWindow, text='next', command=lambda: openConsole(HWID_DEVICE[where]))
     else:
+        comWindow.update()
+
+def connect(portName, bdrate):
+    global serialInst
+    state = 'not connected'
+    comWindow.update()
+    serialInst = serial.Serial(str(portName), int(bdrate))
+    serialInst.port = portName
+    serialInst.open()
+    if not serialInst.is_open():
+        print('serial opening failed')
+        tk.messagebox.showinfo(title= 'error', message= 'opening port failed!')
+        comWindow.update()
+        state = 'cannot be opened'
+        return state
+    else:
+        state = 'successfully opened'
         pass
+    send = 604
+    serialInst.write(bytes(f'{send}'))
+    
+    while serialInst.in_waiting == 0:
+        pass
+    
+    response = serialInst.read(serialInst.in_waiting)
+    print(response)
+    
+    if response == str(send):
+        print('connection success!')
+        state = 'connected'
+    
+    comWindow.update()
+    return state
 
-    canvas = tk.Canvas(windo, width=resolutionX, height=resolutionY)
-    canvas.pack()
-    canvas_image = canvas.create_image(0, 0, anchor=tk.NW)
+def openConsole(port):
+    global comWindow
+    comWindow.destroy()
+    
+    global consl, main
+    consl = tk.Tk()
+    consl.title("Console for Arduino")
+    consl.option_add("*Font","Ariel 15")
+    consl.geometry("1000x500")
+    consl.resizable(False, False)
+    
+    main = tk.Tk()
+    main.title("main program for eagle")
+    main.option_add("*Font","Ariel 15")
+    main.geometry("500x500")
+    main.resizable(True, True)
+    
+    text_widget = tk.Text(consl)
+    text_widget.pack(fill='both', expand= True)
+    
+    # Create the subprocess to run the Arduino CLI and get the serial port name
+    process = subprocess.Popen(['arduino', '--monitor', '--getpref', 'serial.port'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # Get the serial port name from the output of the subprocess
+    serial_port = process.stdout.readline().strip()
+
+    # Create the subprocess to open the Arduino console
+    console = subprocess.Popen(['arduino', '--monitor', f'--port={str(port)}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    def read_from_console():
+        # Read a line of output from the console subprocess
+        line = console.stdout.readline()
+
+        # If the subprocess has terminated, stop reading output
+        if console.poll() is not None:
+            return
+
+        # Append the output to the Tkinter Text widget
+        text_widget.insert('end', line)
+
+        # Schedule the next read in 10 milliseconds
+        consl.after(10, read_from_console)
+
+    # Schedule the first read in 10 milliseconds
+    consl.after(10, read_from_console)
+    comWindow = tk.Text()        
+    
+
+selectedcolours = []
+def detection(vidData, selectedcolours, resolution, ):
+    
+    
 
 
-    # update the video in a loop
-    def update_loop():
-        ret = __camInit__(int(selectedPort), len, resolutionX, resolutionY)
-        update_video(ret, windo, canvas, canvas_image, resolutionX, resolutionY)
-        # call this function again after 10ms
-        windo.after(10, update_loop)
-    update_loop()
-
-                
-        
-def camera_Setting(camPort):
-    global window
-    window.destroy()
-    
-    # create window
-    camWindow = tk.Tk()
-    camWindow.title("Camera Setting")
-    camWindow.option_add("*Font","Ariel 15")
-    camWindow.geometry("1000x500")
-    camWindow.resizable(True, True)
-    
-    # entry
-    infoRes = tk.Label(camWindow, text='Width x Height for camera')
-    infoRes.place(x=30, y=5)
-    leftX = tk.Entry(camWindow, width=4)
-    leftX.setvar('1000')
-    leftX.place(x=30, y=30)
-    rightX = tk.Entry(camWindow, width=4)
-    rightX.setvar('1000')
-    rightX.place(x=90,y=30)
-    x = tk.Label(camWindow, text='X')
-    x.place(x=79, y=33)
-    
-    
-    
-    btn = tk.Button(camWindow, text='search', command=lambda: camDisplayer(camPort, 1, leftX.get(), rightX.get(),windo= camWindow))
-    btn.place(x= 30, y= 67)
-    
-    
-    camWindow.mainloop()
-    
-    
-    
 
 __initiate__()
 
