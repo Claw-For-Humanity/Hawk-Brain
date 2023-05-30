3f5c63a6f4745b4d1a3fed0e0cb3ab4299d9a704
Eric Kang <97197222+erickdhdock@users.noreply.github.com>
2023-05-29 19:27:43 -0700

    update update

--- machineLearn/train&detection.py
+++ machineLearn/train&detection.py
@@ -1,176 +1,27 @@
 import os
 
+mainpath = os.path.join(os.getcwd(), "MachineLearnTest")
+print(mainpath)
+
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
 TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
 LABEL_MAP_NAME = 'label_map.pbtxt'
 
-default_location = os.getcwd()
-
 paths = {
-    'WORKSPACE_PATH': os.path.join(default_location,'Tensorflow', 'workspace'),
-    'SCRIPTS_PATH': os.path.join(default_location,'Tensorflow','scripts'),
-    'APIMODEL_PATH': os.path.join(default_location,'Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join(default_location,'Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join(default_location,'Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join(default_location,'Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join(default_location,'Tensorflow', 'workspace','pre-trained-models'),
-    'CHECKPOINT_PATH': os.path.join(default_location,'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
-    'OUTPUT_PATH': os.path.join(default_location,'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
-    'TFJS_PATH':os.path.join(default_location,'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
-    'TFLITE_PATH':os.path.join(default_location,'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join(default_location,'Tensorflow','protoc')
+    'WORKSPACE_PATH': os.path.join(mainpath, 'Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
  }
 
-files = {
-    'PIPELINE_CONFIG':os.path.join(default_location,'Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
-    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
-}
-
-for path in paths.values():
-    if not os.path.exists(path):
-        if os.name == 'posix':
-            os.makedirs(path)
-        if os.name == 'nt':
-            os.makedirs(path)
-
-def installation():
-
-    os.environ['PATH'] += os.pathsep + os.path.abspath(os.path.join(paths['PROTOC_PATH'],'bin'))
-
-
-    if os.name == 'nt':
-        os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=.')
-        os.system('cd Tensorflow/models/research && copy object_detection\\packages\\tf2\\setup.py setup.py')
-        os.system('cd Tensorflow/models/research && python setup.py build')
-        os.system('cd Tensorflow/models/research && python setup.py install')
-
-        os.system('cd Tensorflow/models/research/slim && pip install -e .')
-        os.system(f'wget {PRETRAINED_MODEL_URL}')
-        os.system(f"move {PRETRAINED_MODEL_NAME}.tar.gz {paths['PRETRAINED_MODEL_PATH']}")
-        os.system(f"cd {paths['PRETRAINED_MODEL_PATH']}")
-        os.system(f'tar -zxvf {PRETRAINED_MODEL_NAME}.tar.gz')
-
-
-
-labels = [{'name':'one', 'id': 1}, {'name':'two', 'id':2}]
-
-with open(files['LABELMAP'], 'w') as f:
-    for label in labels:
-        f.write('item{ \n')
-        f.write(f"\tname:'{label['name']}'\n")
-        f.write(f'\tid:{label["id"]}\n')
-        f.write('}\n')
-
-
-archieve_files = os.path.join(paths['IMAGE_PATH'], 'archieve.tar.gz')
-
-if os.path.exists(archieve_files):
-    os.system(f'tar -zxvf {archieve_files}')
-else:
-    os.system(f"git clone https://github.com/nicknochnack/GenerateTFRecord {paths['SCRIPTS_PATH']}")
-
-os.system(f"python {files['TF_RECORD_SCRIPT']} -x {os.path.join(paths['IMAGE_PATH'], 'train')} -l {files['LABELMAP']} -o {os.path.join(paths['ANNOTATION_PATH'], 'train.record')} ")
-os.system(f"python {files['TF_RECORD_SCRIPT']} -x {os.path.join(paths['IMAGE_PATH'], 'test')} -l {files['LABELMAP']} -o {os.path.join(paths['ANNOTATION_PATH'], 'test.record')} ")
-
-os.system(f"copy {os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'pipeline.config')} {os.path.join(paths['CHECKPOINT_PATH'])}")
-
-
-import tensorflow as tf
-
-from object_detection.utils import config_util
-from object_detection.protos import pipeline_pb2
-from google.protobuf import text_format
-
-config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
-
-print(config)
-
-pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
-
-with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "r") as f:
-    proto_str = f.read()
-    text_format.Merge(proto_str, pipeline_config)
-
-pipeline_config.model.ssd.num_classes = len(labels)
-pipeline_config.train_config.batch_size = 4
-pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
-pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
-pipeline_config.train_input_reader.label_map_path= files['LABELMAP']
-pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
-pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
-pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
-
-config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
-with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:                                                                                                                                                                                                                     
-    f.write(config_text)   
-                                                                             
-TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
-
-command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'])
-
-print(command)
-
-import os
-import tensorflow as tf
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as viz_utils
-from object_detection.builders import model_builder
-from object_detection.utils import config_util
-
-# Load pipeline config and build a detection model
-configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
-detection_model = model_builder.build(model_config=configs['model'], is_training=False)
-
-# Restore checkpoint
-ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-5')).expect_partial()
-
-@tf.function
-def detect_fn(image):
-    image, shapes = detection_model.preprocess(image)
-    prediction_dict = detection_model.predict(image, shapes)
-    detections = detection_model.postprocess(prediction_dict, shapes)
-    return detections
-
-import cv2 
-import numpy as np
-from matplotlib import pyplot as plt
-import matplotlib.pyplot as plt
-
-category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
-
-IMAGE_PATH = os.path.join(paths['IMAGE_PATH'], 'test', 'one.baefe8fb-fa67-11ed-8682-dc41a9eed7f1.jpg')
-
-img = cv2.imread(IMAGE_PATH)
-image_np = np.array(img)
-
-input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-detections = detect_fn(input_tensor)
-
-num_detections = int(detections.pop('num_detections'))
-detections = {key: value[0, :num_detections].numpy()
-              for key, value in detections.items()}
-detections['num_detections'] = num_detections
-
-# detection_classes should be ints.
-detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-
-label_id_offset = 1
-image_np_with_detections = image_np.copy()
-
-viz_utils.visualize_boxes_and_labels_on_image_array(
-            image_np_with_detections,
-            detections['detection_boxes'],
-            detections['detection_classes']+label_id_offset,
-            detections['detection_scores'],
-            category_index,
-            use_normalized_coordinates=True,
-            max_boxes_to_draw=5,
-            min_score_thresh=.8,
-            agnostic_mode=False)
-
-plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
-plt.show()
+print(paths['WORKSPACE_PATH'])
